24286797cf491aa1ddb2b3eaf113a3fb3bafd764
郑宇 <zhengyu@argo-ai.cn>
2021-10-19 15:51:09 +0800

    记录 ts编写

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,14 +1,20 @@
 <template>
 <div class="home">
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <div class="name">{{name}}, {{data.old}}</div>
-  <div class="add-one" @click="addOne">+1</div>
+  <div class="name">name:{{name}}, age:{{age}}, id:{{id}}</div>
+  <div>
+
+    <span class="add-one" @click="addOne">+1</span>
+    <span class="add-one" @click="logId">打印id</span>
+    <!--example-->
+    <span @click="myFun(age)">测试方法</span>
+  </div>
 </div>
 </template>
 
 <script lang="ts">
 import HelloWorld from '@/components/HelloWorld.vue'
-import { defineComponent, ref, reactive, watch, watchEffect } from 'vue'
+import { defineComponent, ref, reactive, watch, watchEffect, computed } from 'vue'
 export default defineComponent(
     {
       name: "home",
@@ -21,16 +27,29 @@ export default defineComponent(
           age: 0,
           id: 0
         })
+        let age = ref(0),
+            id = ref('0')
         // const addOne:Function = (old: number) =>  old + 1
-        const addOne: Function = () =>  data.age += 1
-        watch([data.age, data.id], ([newAge, newId], [oldAge, oldId]) => {
+        const addOne: Function = () => {age.value += 1}
+        const logId = function () {
+          id.value += '1'
+          console.log(id);
+        }
+        const myFun = function (params: any):void {
+          params.value += '1'
+        }
+        watch([age, id], ([newAge, newId], [oldAge, oldId]) => {
           console.log(newAge, oldAge)
           console.log(newId, oldId)
         })
         return {
-          name,
           data,
-          addOne
+          name,
+          age,
+          id,
+          addOne,
+          logId,
+          myFun
         }
       }
     }
@@ -39,10 +58,11 @@ export default defineComponent(
 
 <style lang="stylus" scoped>
 .add-one {
+  display inline-block
   background-color: #000
   color: #fff
-  width: 40px
-  margin: 0 auto
+  width: auto
+  margin: 10px
   cursor pointer
 }
 </style>
