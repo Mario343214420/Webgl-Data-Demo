4bc73fe82a1fac1d5d927d1caba71e790c206223
郑宇 <zhengyu@argo-ai.cn>
2021-11-24 17:24:42 +0800

    提交 GIS瓦片转WEB端，并添加交互

--- src/pages/Channel8/index.vue
+++ src/pages/Channel8/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="channel" ref="channel">
-    <canvas ref="cvs" width="w" height="h"></canvas>
+    <div class="float" :style="`top: ${this.floatPos.y - 140}px; left: ${this.floatPos.x }px; opacity: ${this.flag?'1': '0'}; display: ${this.flag?'block': 'none'}`">
+      <div>地名： {{info.name}}</div>
+      <div>安全状态： {{info.safe}}</div>
+      <div>当地坐标： {{info.coordinate}}</div>
+      <div>当地人数： {{info.count}}</div>
+    </div>
+    <canvas ref="cvs" width="w" height="h" @mousedown="onDocumentMouseDown" @mouseup="flag = false"></canvas>
   </div>
 </template>
 
@@ -23,7 +29,29 @@ export default {
       pipeList: [],
       copyPipe: null,
       group: null,
-      group1: null
+      group1: null,
+      boxes: null,
+      mX: null,
+      mY: null,
+      flag: false,
+      info: {
+        name: '',
+        safe: '安全',
+        coordinate: '模拟坐标{x: 100, y: 200}',
+        count: 100
+      },
+      floatPos: {
+        x: 0,
+        y: 0
+      }
+    }
+  },
+  watch: {
+    mX(n) {
+      console.log(n)
+    },
+    mY(n) {
+      console.log(n)
     }
   },
   mounted() {
@@ -40,50 +68,40 @@ export default {
       const canvas = this.$refs.cvs
       const aspect = this.w/this.h
       this.scene = new THREE.Scene()
-      this.camera = new THREE.PerspectiveCamera(50, aspect, 1, 4000)
-      this.camera.position.z = 1000
-      this.camera.position.y = 1000
+      this.camera = new THREE.PerspectiveCamera(50, aspect, 1, 5000)
+      this.camera.position.z = 2000
+      this.camera.position.x = 0
+      this.camera.position.y = 500
       this.group = new THREE.Group()
       this.group1 = new THREE.Group()
       const glbLoader = new GLTFLoader()
-      /*glbLoader.load('./models/complete_model/keyshot/GLB/aa.glb', glb=>{
-        // let material = new THREE.PointsMaterial({
-        //   color: 0x0000ff, //颜色
-        //   size: 3, //点渲染尺寸
-        // });
-        let material = new THREE.MeshLambertMaterial({
-          color: 0x0000ff
-        });
-        console.log(glb.scene.children[0].geometry)
-        let mesh = new THREE.Mesh(glb.scene.children[0].geometry, material)
-        this.scene.add(mesh)
-        // glb.scene.children[0].position.z = -1000
-        // this.scene.add(glb.scene.children[0])
-      })*/
-      /*glbLoader.load('./models/complete_model/keyshot/GLTF/Tile_+000_002.gltf', glb=>{
-        this.scene.add(glb.scene.children[0])
-      })*/
       let list = [
-        [2,3,4,5,6,7],
-        [2,3,4,5,6,7,8,9],
-        [0,1,2,3,4],
-        [6,7,8,9,10],
-        [2,11],
-        [0,1,2,6,8,10,11],
-        [0,2,3,4,5,6,7,8,9,10,11],
-        [0,1,2,3,4,5,6,7,8,9,10,11],
-        [0,2,3,4,5,6,7,8,9,10,11],
-        [2,3,4,5,6,7,8,9,10],
-        [2,3,4,5,6,7,8,9,10],
-        [3,4,5,6,7,8,9,10],
+        [2,3,4,5,6,7], // 0
+        [2,3,4,5,6,7,8,9], // 1
+        [0,1,2,3,4,5,6,7,8,9,10,11], // 2
+        [0,1,2,3,4,5,6,7,8,9,10,11], // 3
+        [0,1,2,3,4,5,6,7,8,9,10,11], // 4
+        [0,1,2,3,4,5,6,7,8,9,10,11], // 5
+        [0,1,2,3,4,5,6,7,8,9,10,11], // 6
+        [0,1,2,3,4,5,6,7,8,9,10,11], // 7
+        [0,1,2,3,4,5,6,7,8,9,10,11], // 8
+        [0,1,2,3,4,5,6,7,8,9,10,11], // 9
+        [2,3,4,5,6,7,8,9,10], // 10
+        [3,4,5,6,7,8,9,10], // 11
       ]
       list.forEach((item, index) => {
         item.forEach(i => {
           let url = `./models/3dtiles/00${index}/Tile_+00${index}_+00${i}.gltf`
           glbLoader.load(url, glb=>{
             let mesh = glb.scene.children[0]
-            let geo = glb.scene.children[0].geometry
-            // this.group.add(mesh)
+            mesh.material.wireframe = true
+            mesh.position.x += 100
+            mesh.position.y += 100
+            mesh.position.z = -1400
+            // mesh.material.emissive = new THREE.color('#ffffff')
+            let geo = mesh.geometry
+            this.group.add(mesh)
+            this.group.rotation.x = -Math.PI / 2
             let m = new THREE.PointsMaterial({
               color: 0x0000ff, //颜色
               size: 16, //点渲染尺寸
@@ -92,81 +110,53 @@ export default {
           })
         })
       })
-      /*for(let i = 0; i < 6; i++) {
-        let url = `./models/3dtiles/000/Tile_+000_+00${i + 2}.gltf`
-        glbLoader.load(url, glb=>{
-          this.group.add(glb.scene.children[0])
-        })
-      }
-      for(let i = 0; i < 8; i++) {
-        let url = `./models/3dtiles/001/Tile_+001_+00${i + 2}.gltf`
-        glbLoader.load(url, glb=>{
-          this.group.add(glb.scene.children[0])
-        })
-      }
-      for(let i = 0; i < 5; i++) {
-        let url = `./models/3dtiles/002/Tile_+002_+00${i + 2}.gltf`
-        glbLoader.load(url, glb=>{
-          this.group.add(glb.scene.children[0])
-        })
-      }
-      for(let i = 0; i < 5; i++) {
-        let url = `./models/3dtiles/003/Tile_+003_+00${i + 6}.gltf`
-        glbLoader.load(url, glb=>{
-          this.group.add(glb.scene.children[0])
-        })
-      }
-      let url1 = `./models/3dtiles/004/Tile_+004_+002.gltf`
-      let url2 = `./models/3dtiles/004/Tile_+004_+011.gltf`
-      glbLoader.load(url1, glb=>{
-        this.group.add(glb.scene.children[0])
-      })
-      glbLoader.load(url2, glb=>{
-        this.group.add(glb.scene.children[0])
-      })
-      let arr5 = [0,1,2,6,8,10,11]
-      arr5.forEach(item => {
-        let url = `./models/3dtiles/005/Tile_+005_+00${item}.gltf`
-        glbLoader.load(url, glb=>{
-          this.group.add(glb.scene.children[0])
-        })
-      })
-      let arr6 = [0,2,3,4,5,6,7,8,9,10,11]
-      arr6.forEach(item => {
-        let url = `./models/3dtiles/006/Tile_+006_+00${item}.gltf`
-        glbLoader.load(url, glb=>{
-          this.group.add(glb.scene.children[0])
-        })
-      })*/
-
-      // let m = new THREE.PointsMaterial({
-      //   color: 0x0000ff, //颜色
-      //   size: 16, //点渲染尺寸
-      // });
-      // let point = new THREE.Points(geometry, m);
 
       /**
 3dtile.exe -f b3dm -i E:\projects\suligejingchang\set\Data\Tile_+007_+000\Tile_+007_+000.b3dm -o E:\projects\suligejingchang\006\Tile_+007_+000.gltf
 3dtile.exe -f b3dm -i E:\projects\suligejingchang\set\Data\Tile_+008_+000\Tile_+008_+000.b3dm -o E:\projects\suligejingchang\008\Tile_+008_+000.gltf
        */
-      // glbLoader.load('./models/complete_model/keyshot/GLB/钻杆1.glb', glb=>{
-      //   glb.scenes[0].rotateZ(Math.PI/2)
-      //   glb.scenes[0].position.y = 90
-      //   this.pipe = glb.scenes[0]
-      //   this.pipeList = new THREE.Group()
-      // })
 
-      this.scene.add(this.group1)
+      // this.scene.add(this.group1)
+      this.scene.add(this.group)
       const _ambient = new THREE.AmbientLight(0xffffff);
       this.scene.add(_ambient);
 
+      /** 交互用几何体 */
+      this.boxes = new THREE.Group()
+      const g = new THREE.BoxGeometry(100, 100, 100); //创建一个立方体几何对象Geometry
+      const m = new THREE.MeshPhongMaterial({
+        color:0x0000ff,
+        specular:0x4488ee,
+        shininess:12,
+        opacity: 0.2,
+        transparent: true
+      }); //材质对象Material
+      const box = new THREE.Mesh(g, m); //网格模型对象mesh
+      box.position.x = -50
+      box.position.z = -120
+      this.boxes.add(box)
+      let box1 = box.clone(box)
+      box1.position.x = 180
+      box1.position.z = -30
+      let box2 = box.clone(box)
+      box2.position.x = 380
+      box2.position.z = -20
+      box.name = "地点1"
+      box1.name = "地点2"
+      box2.name = "地点3"
+      this.boxes.add(box1)
+      this.boxes.add(box2)
+      this.boxes.add(box)
+      this.scene.add(this.boxes)
+
+
       //灯光属性
-      const _spotLight = new THREE.SpotLight(0xf6f6f6);
+      /*const _spotLight = new THREE.SpotLight(0xf6f6f6);
       _spotLight.castShadow = true;
       _spotLight.position.set(1000,1000,1000);
       //设置阴影贴图精度
       _spotLight.shadow.mapSize.width = _spotLight.shadow.mapSize.height = 1024*4;
-      this.scene.add(_spotLight);
+      this.scene.add(_spotLight);*/
 
       const axesHelper = new THREE.AxesHelper( 20 );
       this.scene.add(axesHelper)
@@ -178,6 +168,39 @@ export default {
       this.controls.update()
       this.renderer.render(this.scene, this.camera)
       requestAnimationFrame(this.animate)
+    },
+    onDocumentMouseDown(e) {
+      e.preventDefault();
+      //将鼠标点击位置的屏幕坐标转成threejs中的标准坐标,具体解释见代码释义
+      this.mX = (e.clientX / window.innerWidth) * 2 - 1;
+      this.mY = -(e.clientY / window.innerHeight) * 2 + 1;
+      this.floatPos = {
+        x: e.offsetX,
+        y: e.offsetY
+      }
+      //新建一个三维单位向量 假设z方向就是0.5
+      //根据照相机，把这个向量转换到视点坐标系
+      let vector = new THREE.Vector3(this.mX, this.mY,0.5).unproject(this.camera);
+
+      //在视点坐标系中形成射线,射线的起点向量是照相机， 射线的方向向量是照相机到点击的点，这个向量应该归一标准化。
+      let raycaster = new THREE.Raycaster();
+      raycaster.setFromCamera( {x: this.mX, y: this.mY}, this.camera );
+
+      const intersects = raycaster.intersectObjects( this.boxes.children );
+      if(intersects[0].object.name){
+        this.flag = true
+        this.info.name = intersects[0].object.name
+        this.info.count = parseInt(Math.random() * 30)
+      }
+      /*//射线和模型求交，选中一系列直线
+      var intersects = raycaster.intersectObjects(objects);
+      console.log('imtersrcts=' + intersects)
+      if (intersects.length > 0) {
+        //选中第一个射线相交的物体
+        SELECTED = intersects[0];
+        var intersected = intersects[0];
+        console.log(intersects[0])
+      }*/
     }
   }
 }
@@ -188,5 +211,17 @@ export default {
   display block
   width: 100%
   height: 100%
+  position: relative
+  .float {
+    position: absolute
+    opacity: 0
+    z-index 100
+    background-color: #fff
+    border: 1px solid #000
+    -webkit-border-radius: 6px
+    -moz-border-radius: 6px
+    border-radius: 6px
+    padding: 20px
+  }
 }
 </style>
