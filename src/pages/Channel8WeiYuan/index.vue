29a3b9f99adae596170df7b2b83d63b16ec06f84
郑宇 <zhengyu@argo-ai.cn>
2022-02-08 09:06:54 +0800

    提交 动态添加模型

--- src/pages/Channel8WeiYuan/index.vue
+++ src/pages/Channel8WeiYuan/index.vue
@@ -19,6 +19,7 @@ import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
 import { BloomPass } from 'three/examples/jsm/postprocessing/BloomPass'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { CopyShader } from 'three/examples/jsm/shaders/CopyShader'
+import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer'
 export default {
   name: 'Channel8WeiYuan',
   data() {
@@ -81,7 +82,7 @@ export default {
       this.group = new THREE.Group()
       this.groupController = new THREE.Group()
       const fbxLoader = new FBXLoader()
-      /*let list = [
+      let list = [
         // "Tile_+000_+000",
         // "Tile_+000_+001",
         // "Tile_+000_+002",
@@ -202,8 +203,8 @@ export default {
         // "Tile_+009_+009",
         // "Tile_+009_+010",
         // "Tile_+009_+011"
-      ]*/
-      let list = [
+      ]
+      /*let list = [
         "Tile_+000_+000",
         "Tile_+000_+001",
         "Tile_+000_+002",
@@ -324,9 +325,9 @@ export default {
         "Tile_+009_+009",
         "Tile_+009_+010",
         "Tile_+009_+011"
-      ]
+      ]*/
       list.forEach((item, index) => {
-        let url = `http://192.168.1.195:8000/${item}/${item}.FBX`
+        let url = `http://192.168.1.33:8000/${item}/${item}.FBX`
         fbxLoader.load(url, fbx => {
           this.group.add(fbx)
         })
@@ -353,7 +354,6 @@ export default {
         mesh.position.set(0.9,-5.7,-40.85)
         mesh.visible = false
         mesh.name = '录井房'
-        console.log(mesh)
         this.groupController.add(mesh)
       })
       this.group.position.y = this.groupController.position.y = 45
@@ -366,9 +366,11 @@ export default {
       this.scene.add(_ambient);
 
       const axesHelper = new THREE.AxesHelper( 20 );
-      this.scene.add(axesHelper)
+      // this.scene.add(axesHelper)
       this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: false })
       this.renderer.setSize(this.w, this.h)
+      // 炫光特效关键代码 ***
+      this.renderer.autoClear = false
       this.controls = new OrbitControls(this.camera, this.renderer.domElement)
       this.controls.enableDamping = true
       this.addBloomPass()
@@ -380,25 +382,6 @@ export default {
       this.controls.update()
       this.renderer.render(this.scene, this.camera)
       requestAnimationFrame(this.animate)
-    },
-    addBloomPass() {
-      // RenderPass这个通道会渲染场景，但不会将渲染结果输出到屏幕上
-      const renderScene = new RenderPass(this.scene, this.camera)
-      const effectCopy = new ShaderPass(CopyShader); //传入了CopyShader着色器，用于拷贝渲染结果
-      effectCopy.renderToScreen = true;
-      // THREE.BloomPass(strength, kernelSize, sigma, Resolution)
-      // strength 定义泛光效果的强度，值越高，明亮的区域越明亮，而且渗入较暗区域的也就越多
-      // kernelSize 控制泛光的偏移量
-      // sigma 控制泛光的锐利程度，值越高，泛光越模糊
-      // Resolution 定义泛光的解析图，如果该值太低，结果的方块化就会越严重
-      // const bloomPass = new BloomPass(3, 1.5, 0.4, 1024); //BloomPass通道效果
-      const bloomPass = new BloomPass(1.2, 25, 4.0, 256); //BloomPass通道效果
-      //创建效果组合器对象，可以在该对象上添加后期处理通道，通过配置该对象，使它可以渲染我们的场景，并应用额外的后期处理步骤，在render循环中，使用EffectComposer渲染场景、应用通道，并输出结果。
-      this.bloomComposer = new EffectComposer(this.renderer)
-      this.bloomComposer.setSize(window.innerWidth, window.innerHeight);
-      this.bloomComposer.addPass(renderScene);
-      this.bloomComposer.addPass(bloomPass);
-      this.bloomComposer.addPass(effectCopy);
     },
     onDocumentMouseDown(e) {
       e.preventDefault();
@@ -488,6 +471,25 @@ export default {
       sprite.renderOrder = 4
       this.group.add(line)
       this.group.add(sprite)
+    },
+    addBloomPass() {
+      // RenderPass这个通道会渲染场景，但不会将渲染结果输出到屏幕上
+      const renderScene = new RenderPass(this.scene, this.camera)
+      const effectCopy = new ShaderPass(CopyShader); //传入了CopyShader着色器，用于拷贝渲染结果
+      effectCopy.renderToScreen = true;
+      // THREE.BloomPass(strength, kernelSize, sigma, Resolution)
+      // strength 定义泛光效果的强度，值越高，明亮的区域越明亮，而且渗入较暗区域的也就越多
+      // kernelSize 控制泛光的偏移量
+      // sigma 控制泛光的锐利程度，值越高，泛光越模糊
+      // Resolution 定义泛光的解析图，如果该值太低，结果的方块化就会越严重
+      // const bloomPass = new BloomPass(3, 1.5, 0.4, 1024); //BloomPass通道效果
+      const bloomPass = new BloomPass(0.8, 25, 4.0, 256); //BloomPass通道效果
+      //创建效果组合器对象，可以在该对象上添加后期处理通道，通过配置该对象，使它可以渲染我们的场景，并应用额外的后期处理步骤，在render循环中，使用EffectComposer渲染场景、应用通道，并输出结果。
+      this.bloomComposer = new EffectComposer(this.renderer)
+      this.bloomComposer.setSize(this.w, this.h);
+      this.bloomComposer.addPass(renderScene);
+      this.bloomComposer.addPass(bloomPass);
+      this.bloomComposer.addPass(effectCopy);
     }
   }
 }
